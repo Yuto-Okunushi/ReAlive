e716a397e2fa4ca424ff86e11abbfdb783ec513a
Yuto <yuto_1007@yahoo.ne.jp>
2024-06-05 15:12:37 +0900

    スクリプトの文章修正

--- Assets/Demo/Scripts/Buttons/ButtonselectCheck.cs
+++ Assets/Demo/Scripts/Buttons/ButtonselectCheck.cs
@@ -4,25 +4,16 @@ using UnityEngine;
 
 public class ButtonselectCheck : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void StartButtonCheck()
     {
         Debug.Log("スタートボタンが選択されました");
+        SceneController.LoadNextScene("Demo_Scenes");
     }
 
     public void EndButtonCheck()
     {
         Debug.Log("エンドボタンが選択されました");
+        Application.Quit();
     }
 }
