8c1999acb97ed870834f9cc8c39c717e1ebe1b15
close_farm <zkx5emc8c968q966knhr@icloud.com>
2024-06-11 03:50:52 +0900

    コンフリクト修正

--- Assets/Demo/Scripts/HattaScript/DisasterSystem.cs
+++ Assets/Demo/Scripts/HattaScript/DisasterSystem.cs
@@ -39,8 +39,10 @@ public class DisasterSystem : MonoBehaviour
         {
             // 準備フェーズの開始
             yield return StartCoroutine(Prep());
+
             // 災害発生フェーズ
             yield return StartCoroutine(Disaster());
+
             // 避難フェーズの開始
             yield return StartCoroutine(Evac());
         }
@@ -57,8 +59,8 @@ public class DisasterSystem : MonoBehaviour
         // 準備フェーズのアニメーションを開始
         yield return StartCoroutine(PanelUI.Instance.PrepAnim());
 
-        // ランダムな時間待機 (3分から4分の間)
-        float randomTime = UnityEngine.Random.Range(180f, 240f);
+        // ランダムな時間待機 (10秒から24秒の間)
+        float randomTime = UnityEngine.Random.Range(10f, 24f); // 変更要必須
         yield return new WaitForSeconds(randomTime);
 
         // 災害発生をトリガー
