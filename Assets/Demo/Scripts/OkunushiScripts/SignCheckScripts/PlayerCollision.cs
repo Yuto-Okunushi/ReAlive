3e7495ee3ea05fc6087a571b0a8ba74729cf4a12
Yuto <yuto_1007@yahoo.ne.jp>
2024-07-15 17:50:19 +0900

    UIの配置とゲームマネージャーにデータを送るシステム制作
    バイトなので途中までの経過でプッシュします
    あとは標識オブジェクトにあたった時に配置したUIに画像とテキストデータを反映させるだけです

--- Assets/Demo/Scripts/OkunushiScripts/SignCheckScripts/PlayerCollision.cs
+++ Assets/Demo/Scripts/OkunushiScripts/SignCheckScripts/PlayerCollision.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    [SerializeField] SignDate[] signDate;
+
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Signs")
+        if (other.gameObject.tag == "Sign1")
         {
-            Debug.Log("•W¯‚É‚Ô‚Â‚©‚è‚Ü‚µ‚½");
+            Debug.Log("•W¯1‚É‚Ô‚Â‚©‚è‚Ü‚µ‚½");
+            SendSignDate(0);
+        }
+        else if (other.gameObject.tag == "Sign2")
+        {
+            Debug.Log("•W¯2‚É‚Ô‚Â‚©‚è‚Ü‚µ‚½");
+            SendSignDate(1);
+        }
+    }
+
+    private void SendSignDate(int index)
+    {
+        if (index >= 0 && index < signDate.Length)
+        {
+            GameManager.SetSignDate(signDate[index]);
         }
         else
         {
-
+            Debug.LogError("Invalid index for signDate array");
         }
     }
 }
